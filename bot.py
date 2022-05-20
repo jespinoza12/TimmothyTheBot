5361820a1a5436162ffa666ff307a4f3963b34e4
jespinoza12 <jespinoza@student.neumont.edu>
2022-05-20 17:29:05 -0600

    fix

--- bot.py
+++ bot.py
@@ -51,88 +51,88 @@ class Bot(commands.Bot):
     async def test(self, ctx: commands.Context):
         await ctx.send(f'{self.nick} has landed')
 
-@bot.command(name='questions')
-async def questions(ctx):
-    await ctx.send('How long have you been streaming? (!Long)' + '\n' +
-                    "What's My Name? (!Name)" + '\n' + "How Old Am I? (!Age)" + '\n' + "Where Am I from? (!From)" + '\n' + "When Do I Stream? (!Stream)")
+    @commands.command(name='questions')
+    async def questions(ctx):
+        await ctx.send('How long have you been streaming? (!Long)' + '\n' +
+                        "What's My Name? (!Name)" + '\n' + "How Old Am I? (!Age)" + '\n' + "Where Am I from? (!From)" + '\n' + "When Do I Stream? (!Stream)")
 
-@bot.command(name='Long')
-async def Long(ctx):
-    await ctx.send('I have been streaming for 2 years')
+    @commands.command(name='Long')
+    async def Long(ctx):
+        await ctx.send('I have been streaming for 2 years')
 
-@bot.command(name='Name')
-async def Name(ctx):
-    await ctx.send('Julian Winters')
+    @commands.command(name='Name')
+    async def Name(ctx):
+        await ctx.send('Julian Winters')
 
-@bot.command(name='Age')
-async def Age(ctx):
-    await ctx.send('20')
+    @commands.command(name='Age')
+    async def Age(ctx):
+        await ctx.send('20')
 
-@bot.command(name='From')
-async def From(ctx):
-    await ctx.send('I am from New York, Carliona')
+    @commands.command(name='From')
+    async def From(ctx):
+        await ctx.send('I am from New York, Carliona')
 
-@bot.command(name='Stream')
-async def Stream(ctx):
-    await ctx.send('I stream everyday at 4')
+    @commands.command(name='Stream')
+    async def Stream(ctx):
+        await ctx.send('I stream everyday at 4')
 
-@bot.command(name='beginraffle')
-async def beginraffle(ctx):
-    await ctx.send("Entries for the raffle have started. Type !raffle  to join now!!")
-    channelname = 'fro7yfeet' #Set the channel name here (No need for # that is done on for you)
-    nick = os.environ['BOT_NICK']  # create an account for your bot on twitch then set you bot name here
-    password = 'uf075iioydjehomm7ni6kfborwdcmi' #get your bots oath from http://www.twitchapps.com/tmi/
-    # Do Not change anything below unless you know what your doing
+    @commands.command(name='beginraffle')
+    async def beginraffle(ctx):
+        await ctx.send("Entries for the raffle have started. Type !raffle  to join now!!")
+        channelname = 'fro7yfeet' #Set the channel name here (No need for # that is done on for you)
+        nick = os.environ['BOT_NICK']  # create an account for your bot on twitch then set you bot name here
+        password = 'uf075iioydjehomm7ni6kfborwdcmi' #get your bots oath from http://www.twitchapps.com/tmi/
+        # Do Not change anything below unless you know what your doing
 
-    queue = 13 
-    channel = '#'+channelname
-    server = 'irc.twitch.tv'
-    irc = socket.socket()
-    irc.connect((server, 6667)) 
-    irc.send('PASS ' + password + '\r\n')
-    irc.send('NICK ' + nick + '\r\n')
-    irc.send('JOIN ' + channel + '\r\n')
-    rafflelist = []
-    beginraffle = "Entries for the raffle have started. Type !raffle  to join now!!"
-    print (beginraffle);
-    irc.send('PRIVMSG ' + channel + ' :' + beginraffle + '\r\n')
-    def rafflesave():
-        rafflelist.append(user)
-    def run_raffle():
-        print (rafflelist)
-        winner = random.choice(rafflelist)
-        rafflewinner = winner + " is the winner!! :)"
-        irc.send('PRIVMSG ' + os.environ['CHANNEL'] + ' :' + rafflewinner + '\r\n')
-        print (winner) + ' won the raffle!!!'
-        os._exit(0)
-    def message(msg):
-        global queue
-        queue = 5
-        if queue < 20: 
-            irc.send('PRIVMSG ' + channel + ' :' + msg + '\r\n')
-        else:
-            print ('Message deleted')
-    def queuetimer(): 
-        global queue
-        queue = 0
-        threading.Timer(30,queuetimer).start()
-    queuetimer()
-    while True:
-        tilthack = irc.recv(1204)
-        user = tilthack.split(':')[1]
-        user = user.split('!')[0] 
-        if tilthack.find('PING') != -1:
-            irc.send(tilthack.replace('PING', 'PONG')) 
-        if tilthack.find('!raffle') != -1: 
-                if any(word in user for word in rafflelist):
-                    message(user + ' has already entered :)')
-                else:
-                    rafflesave()
-                    message(user + ' has been added to the raffle :) '+ str(len(rafflelist)) + ' user(s) have joined the raffle.')
-                    print (rafflelist) 
-                    print (len(rafflelist)), 'user(s) have joined'
-        if tilthack.find('!runraffle') != -1:
-            run_raffle()
+        queue = 13 
+        channel = '#'+channelname
+        server = 'irc.twitch.tv'
+        irc = socket.socket()
+        irc.connect((server, 6667)) 
+        irc.send('PASS ' + password + '\r\n')
+        irc.send('NICK ' + nick + '\r\n')
+        irc.send('JOIN ' + channel + '\r\n')
+        rafflelist = []
+        beginraffle = "Entries for the raffle have started. Type !raffle  to join now!!"
+        print (beginraffle);
+        irc.send('PRIVMSG ' + channel + ' :' + beginraffle + '\r\n')
+        def rafflesave():
+            rafflelist.append(user)
+        def run_raffle():
+            print (rafflelist)
+            winner = random.choice(rafflelist)
+            rafflewinner = winner + " is the winner!! :)"
+            irc.send('PRIVMSG ' + os.environ['CHANNEL'] + ' :' + rafflewinner + '\r\n')
+            print (winner) + ' won the raffle!!!'
+            os._exit(0)
+        def message(msg):
+            global queue
+            queue = 5
+            if queue < 20: 
+                irc.send('PRIVMSG ' + channel + ' :' + msg + '\r\n')
+            else:
+                print ('Message deleted')
+        def queuetimer(): 
+            global queue
+            queue = 0
+            threading.Timer(30,queuetimer).start()
+        queuetimer()
+        while True:
+            tilthack = irc.recv(1204)
+            user = tilthack.split(':')[1]
+            user = user.split('!')[0] 
+            if tilthack.find('PING') != -1:
+                irc.send(tilthack.replace('PING', 'PONG')) 
+            if tilthack.find('!raffle') != -1: 
+                    if any(word in user for word in rafflelist):
+                        message(user + ' has already entered :)')
+                    else:
+                        rafflesave()
+                        message(user + ' has been added to the raffle :) '+ str(len(rafflelist)) + ' user(s) have joined the raffle.')
+                        print (rafflelist) 
+                        print (len(rafflelist)), 'user(s) have joined'
+            if tilthack.find('!runraffle') != -1:
+                run_raffle()
 
 bot = Bot()
 bot.run()
