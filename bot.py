6fcf0dc44f0a3561026003cba5e52b6f6dea0790
jespinoza12 <jespinoza@student.neumont.edu>
2022-05-27 01:17:02 -0600

    Fixed Raffle

--- bot.py
+++ bot.py
@@ -1,29 +1,25 @@
 from ast import keyword
+from cProfile import run
+from pickle import TRUE
 import socket,threading,random,os
 from time import time
+from typing import Counter
 from unicodedata import name
 from twitchio.ext import commands, eventsub
 
 class Bot(commands.Bot):
     def __init__(self):
-        # Initialise our Bot with our access token, prefix and a list of channels to join on boot...
-        # prefix can be a callable, which returns a list of strings or a string...
-        # initial_channels can also be a callable which returns a list of strings...
         super().__init__(token=os.environ['TMI_TOKEN'], prefix=os.environ['BOT_PREFIX'], initial_channels=[os.environ['CHANNEL']])
-
+    global run
     async def event_message(self, message):
-        # Messages with echo set to True are messages sent by the bot...
-        # For now we just want to ignore them...
         if message.echo:
             return
 
-        # Print the contents of our message to console...
         print(message.content)
         keywords = []
         my_file = open("badwords.txt", "r")
         keywords = my_file.read()
 
-        #Checks for specific words and deletes it and times user out
         if(message.content in keywords) : 
             message_id = message.tags['id']
             timeout = 120
@@ -31,21 +27,12 @@ class Bot(commands.Bot):
             await message.channel.send(f"Thats a badword {message.author.name} so your message has been deleted")
             await message.channel.send(f"/delete {message_id}")
             await message.channel.send(f"/timeout {message.author.name} {timeout} {reason}]")
-
-        # Since we have commands and are overriding the default `event_message`
-        # We must let the bot know we want to handle and invoke our commands...
         await self.handle_commands(message)
 
 
     #Says Hello back
     @commands.command()
     async def hello(self, ctx: commands.Context):
-        # Here we have a command hello, we can invoke our command with our prefix and command name
-        # e.g ?hello
-        # We can also give our commands aliases (different names) to invoke with.
-
-        # Send a hello back!
-        # Sending a reply back to the channel is easy... Below is an example.
         await ctx.send(f'Hello {ctx.author.name}!')
 
 
@@ -99,6 +86,8 @@ class Bot(commands.Bot):
     async def beginraffle(self, ctx: commands.Context):
         print(ctx.author.name)
         if('fros7yfeet' == ctx.author.name):
+            my_file1 = open("run.txt", "w")
+            my_file1.write("True")
             await ctx.send('Entries for the raffle have started. Type !raffle  to join now!!')
         else:
             await ctx.send('You are not the Streamer')
@@ -106,29 +95,40 @@ class Bot(commands.Bot):
     @commands.command()
     async def raffle(self, ctx: commands.Context):
             print(ctx.author.name)
-            my_file1 = open("listForRaffle.txt", "r")
-            if (ctx.author.name not in my_file1) :
-                my_file1 = open("listForRaffle.txt", "a")
-                my_file1.write(f"{ctx.author.name} \n")
-                my_file1.close()
-                await ctx.send(f"{ctx.author.name} has entered the raffle!")
+            my_file1 = open("run.txt", "r")
+            my_file3 = open("listForRaffle.txt", "r")
+            
+            if (my_file1.read() == "True"):
+                if (ctx.author.name not in my_file3.read()) :
+                    my_file1 = open("listForRaffle.txt", "a")
+                    my_file1.write(f"{ctx.author.name} \n")
+                    my_file1.close()
+                    await ctx.send(f"{ctx.author.name} has entered the raffle!")
+                else :
+                    await ctx.send(f"/w {ctx.author.name} You have already entered this raffle!")
             else :
-                ctx.send(f"{ctx.author.name} you have already entered this raffle")
+                await ctx.send("Raffle has not started")
     
     @commands.command()
     async def endraffle(self, ctx: commands.Context):
         print(ctx.author.name)
-        if(ctx.author.name == "fros7yfeet"):
-            my_file1 = open("listForRaffle.txt", "r")
-            lines= my_file1.read().split("\n")
-            my_file1.close()
-            random_person = random.choice(lines)
-            print(random_person)
-            await ctx.send(f"{random_person} has won the raffle")
-            with open("listForRaffle.txt", "w") as f:
-                f.write("")
-        else:
-           await ctx.send('You are not the Streamer')
+        my_file1 = open("run.txt", "r")
+        if(my_file1.read() == "True"):
+            if(ctx.author.name == "fros7yfeet"):
+                my_file1 = open("listForRaffle.txt", "r")
+                lines= my_file1.read().split("\n")
+                my_file1.close()
+                random_person = random.choice(lines)
+                print(random_person)
+                await ctx.send(f"{random_person} has won the raffle")
+                with open("listForRaffle.txt", "w") as f:
+                    f.write("")
+                with open("run.txt", "w") as f:
+                    f.write("False")
+            else:
+                await ctx.send("You are not the Streamer")
+        else: 
+            await ctx.send("Raffle has not started")
 
 bot = Bot()
 bot.run()
